abc3c1bed34f74f594df6243e9a9076beb41d479
Hyunjiiing <h.jilluminate@gmail.com>
2023-01-03 18:08:30 +0900

    Submit web hw 1

---
+++ Homework/Web-homework-1/script.js
@@ -0,0 +1,26 @@
+var idx = document.getElementById("idx");
+var ent = document.getElementById("ent");
+
+const config = {
+  method: "get",
+};
+
+ent.addEventListener("click", function () {
+  fetch(
+    "https://my-json-server.typicode.com/smashh712/json_placeholder/members/",
+    config
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      var result = document.getElementById("result");
+      result.innerText =
+        data[idx.value].name +
+        "님의 생일은 " +
+        data[idx.value].birth.slice(0, 2) +
+        "년 " +
+        data[idx.value].birth.slice(2, 4) +
+        "월 " +
+        data[idx.value].birth.slice(4, 6) +
+        "일 입니다";
+    });
+});
