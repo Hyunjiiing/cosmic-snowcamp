018136f726f5f4541ef7cd165f044b9d7577da07
Hyunjiiing <h.jilluminate@gmail.com>
2023-01-11 19:39:59 +0900

    Fix web hw2 - js file

--- Homework/Web-homework-2/script.js
+++ Homework/Web-homework-2/script.js
@@ -1,25 +1,28 @@
 pink = document.getElementById("pink");
 gray = document.getElementById("gray");
-text = document.getElementById("text"); // 텍스트필드st
-save = document.getElementById("save"); // 저장 버튼
-board = document.getElementById("board"); // 메모지 보드
+text = document.getElementById("text");
+ent = document.getElementById("enter");
+temp = document.getElementById("board");
 
 var i = 0;
-save.addEventListener("click", () => {
-  //버튼이 눌리면 밑 함수가 실행되면서 저장됨
+ent.addEventListener("click", () => {
   let memo = document.createElement("div");
-  let title = document.createElement("div"); // title = 제목 (날짜)
-  let content = document.createElement("div"); // content = 내용 + 버튼
-  let script = document.createElement("div"); //script = 내용
+  let title = document.createElement("div");
+  let content = document.createElement("div");
+  let script = document.createElement("div");
   let erase = document.createElement("input");
 
-  title.innerHTML = new Date();
+  let titleString = document.createTextNode(new Date());
+  title.appendChild(titleString);
+
+  let scriptString = document.createTextNode(text.value);
+  script.appendChild(scriptString);
+
   if (pink.checked) {
     title.style.backgroundColor = "pink";
   } else if (gray.checked) {
     title.style.backgroundColor = "gray";
   }
-  script.innerHTML = text.value; // 입력한거 가져와서 보여지게
   memo.setAttribute("id", "memo" + i);
   erase.setAttribute("type", "button");
   erase.setAttribute("value", "삭제");
@@ -35,11 +38,9 @@ save.addEventListener("click", () => {
     let eraseDiv = document.getElementById("memo" + erase.id);
     eraseDiv.remove();
   });
-
-  //DOM구조 합췌~~
-  content.appendchild(script);
-  content.appendchild(erase);
-  memo.appendchild(title);
-  memo.appendchild(content);
-  board.appendchild(memo);
+  content.appendChild(script);
+  content.appendChild(erase);
+  memo.appendChild(title);
+  memo.appendChild(content);
+  temp.appendChild(memo);
 });
