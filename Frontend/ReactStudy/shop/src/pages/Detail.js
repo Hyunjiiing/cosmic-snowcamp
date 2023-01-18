7d3a2ab962c00f57346fe4ac991ba282a185f2dc
Hyunjiiing <h.jilluminate@gmail.com>
2023-01-18 21:03:20 +0900

    Follow Coding Apple's React lecture code lecture topic : useEffect, ajax

--- Frontend/ReactStudy/shop/src/pages/Detail.js
+++ Frontend/ReactStudy/shop/src/pages/Detail.js
@@ -1,4 +1,20 @@
-function DetailPage() {
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+function DetailPage(props) {
+  let [num, setNum] = useState("");
+  useEffect(() => {
+    if (isNaN(num) == true) {
+      <p>"숫자를 입력하세요"</p>;
+    }
+  }, [num]);
+
+  console.log("안녕");
+
+  let [count, setCount] = useState(0);
+
+  let { id } = useParams();
+
   return (
     <div>
       <div className="container">
@@ -10,9 +26,9 @@ function DetailPage() {
             />
           </div>
           <div className="col-md-6">
-            <h4 className="pt-5">상품명</h4>
-            <p>상품설명</p>
-            <p>120000원</p>
+            <h4 className="pt-5">{props.shoes[id].title}</h4>
+            <p>{props.shoes[id].content}</p>
+            <p>{props.shoes[id].price}</p>
             <button className="btn btn-danger">주문하기</button>
           </div>
         </div>
